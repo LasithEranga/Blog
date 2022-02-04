1ea10a6340b3df4c325da768faa466cf0bef73b4
LasithEranga <lasitheranga1@gmail.com>
2022-02-05 03:12:18 +0530

    Featured post and post pages

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const bodyPaser = require('body-parser');
 const ejs = require("ejs");
+const featuredPost = [];
+const homeContents = [];
+const _ = require('lodash');
+
 
 app.set('view engine', 'ejs');
 app.use(bodyPaser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+
+
+//setting data for featured post
+featuredPost.push({title:"MDN Web Docs", content:"MDN Web Docs, previously Mozilla Developer Network and formerly Mozilla Developer Center, is a documentation repository and learning resource for web developers used by Mozilla, Microsoft, Google, and Samsung."})
 app.get('/', (req, res)=>{
-    res.render('index');
+    res.render('index', {featuredPost:featuredPost});
 })
 
 app.listen(3000, ()=>{
